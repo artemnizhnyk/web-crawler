fdb189bb0a9587c0e17ebba541fcbe8a78da1007
Artem Nizhnyk <nizhnyk4253@gmail.com>
2023-12-25 12:38:22 +0100

    Initial commit

---
+++ src/main/java/com/artemnizhnyk/Crawler.java
@@ -0,0 +1,2 @@
+package com.artemnizhnyk;public class Crawler {
+}
---
+++ src/test/java/com/artemnizhnyk/WebCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
